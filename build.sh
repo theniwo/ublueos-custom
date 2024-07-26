2d04f426f38402dc009ce9a947c9e604ad62452e
theniwo <git@theniwo.de>
2024-07-26 19:27:19 +0200

    Added displaylink-driver

--- build.sh
+++ build.sh
@@ -13,11 +13,10 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install tmux screen byobu htop bpytop vim-enhanced
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+./displaylink-driver-6.0.0-24.run --confirm --quiet --accept --noprogress
