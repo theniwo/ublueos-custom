39be3018d9db991b4ff98c05aa960ec6f84cb294
theniwo <git@theniwo.de>
2024-07-27 19:26:51 +0200

    Changed to Bluefin

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install tmux screen byobu htop bpytop vim-enhanced vlc libgda libgda-sqlite guake libvirt virt-manager virt-viewer
+rpm-ostree install tmux screen byobu htop bpytop vim-enhanced vlc libgda libgda-sqlite guake libvirt virt-manager nmap
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
