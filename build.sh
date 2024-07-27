27db2a65c65425fa0eec30e8ea3d78ed088ee22e
theniwo <git@theniwo.de>
2024-07-27 15:56:16 +0200

    feat: :sparkles: libvirt & virtmanager

--- build.sh
+++ build.sh
@@ -13,11 +13,11 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install tmux screen byobu htop bpytop vim-enhanced vlc libgda libgda-sqlite
+rpm-ostree install tmux screen byobu htop bpytop vim-enhanced vlc libgda libgda-sqlite guake libvirt virt-manager virt-viewer
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
-#systemctl enable podman.socket
+systemctl enable podman.socket
